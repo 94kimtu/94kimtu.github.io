32e107d22b06249e7a4ca77cc12a4ecfa0d604b0
kim ty <tykim@scgs.co.kr>
2024-08-19 13:07:03 +0900

    @94kimtu/ 기초

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Footer from "./components/Footer";
 
 function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
