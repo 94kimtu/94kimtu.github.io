2feca84f07333cc345deab58ee1c21ef046b8e09
94kimtu <taeyoo94@naver.com>
2024-08-21 22:00:37 +0900

    @94kimtu/ 수정

--- src/assets/Icons.ts
+++ src/assets/Icons.ts
@@ -1,8 +1,6 @@
-import { ReactComponent as IconsLogoBBRW } from "./icons/logo.svg";
 import { ReactComponent as IconsLogoW } from "./icons/logoW.svg";
 import { ReactComponent as IconsLogoB } from "./icons/logoB.svg";
 const Icons = {
-    IconsLogoBBRW,
     IconsLogoW,
     IconsLogoB,
 };
--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -3,21 +3,35 @@ import React from "react";
 import styled from "styled-components";
 // import Icons from "../assets/Icons";
 const AsideStyle = styled.section`
+    position: fixed;
+    bottom: 50px;
+    right: 50px;
+    z-index: 1;
     .to-top-bbutton-area {
-        width: 100px;
-        height: 100px;
+        width: 50px;
+        height: 50px;
         position: relative;
         z-index: 1;
-        transition: 0.3s;
         margin: auto;
         transform-style: preserve-3d;
+        transition: 0.5s;
+        :hover {
+            transition: 5s;
+            rotate: 0.1 1 0.1 360deg;
+        }
+        :active {
+            transition: 0.5s;
+            rotate: 0 0 0 360deg;
+        }
         .to-top-bbutton-box {
+            user-select: none;
+            position: relative;
             position: absolute;
             width: 100%;
             height: 100%;
-            translate: 0 0 0px;
-            transform-style: preserve-3d;
-            transform: translateZ(50px);
+            transition: 0.5s;
+            transform: translateZ(25px);
+            cursor: pointer;
             &.front {
                 rotate: 0 1 0 0deg;
                 background-color: #00000066;
@@ -43,10 +57,19 @@ const AsideStyle = styled.section`
                 background-color: #00000066;
             }
             .to-top-bbutton {
+                width: 20px;
+                height: 2px;
+                background-color: #ffffffdd;
+                position: absolute;
+                top: 22px;
+                left: 22px;
+                transition: 0.3s;
+                transform: rotate(45deg);
+                &.first {
+                    transform: rotate(-45deg);
+                    left: 8px;
+                }
             }
-        }
-        :hover {
-            rotate: 1 1 1 45deg;
         }
     }
 `;
@@ -55,27 +78,27 @@ const Aside = () => {
         <AsideStyle>
             <aside className="to-top-bbutton-area">
                 <section className="to-top-bbutton-box front">
-                    <article className="to-top-bbutton first">front</article>
+                    <article className="to-top-bbutton first"></article>
                     <article className="to-top-bbutton second"></article>
                 </section>
                 <section className="to-top-bbutton-box back">
-                    <article className="to-top-bbutton first">back</article>
+                    <article className="to-top-bbutton first"></article>
                     <article className="to-top-bbutton second"></article>
                 </section>
                 <section className="to-top-bbutton-box right">
-                    <article className="to-top-bbutton first">right</article>
+                    <article className="to-top-bbutton first"></article>
                     <article className="to-top-bbutton second"></article>
                 </section>
                 <section className="to-top-bbutton-box left">
-                    <article className="to-top-bbutton first">left</article>
+                    <article className="to-top-bbutton first"></article>
                     <article className="to-top-bbutton second"></article>
                 </section>
                 <section className="to-top-bbutton-box top">
-                    <article className="to-top-bbutton first">top</article>
+                    <article className="to-top-bbutton first"></article>
                     <article className="to-top-bbutton second"></article>
                 </section>
                 <section className="to-top-bbutton-box bottom">
-                    <article className="to-top-bbutton first">bottom</article>
+                    <article className="to-top-bbutton first"></article>
                     <article className="to-top-bbutton second"></article>
                 </section>
             </aside>
